3db5d7c066a38dbf3c641b9b13e193fcd1b87331
Soldy <4786022+Soldy@users.noreply.github.com>
2020-09-19 17:08:41 +0100

    setup interface fix
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 const typeHardening =  new (require('typehardeningrc')).typeHardeningBase();
 
 const setupBase = function (setupIn){
+    /*
+     * get option value
+     * @param {string} value
+     * @public
+     * @return {ann}
+     */
     this.get = function(type){
         if(typeof type === 'undefined')
             return false;
@@ -10,11 +16,26 @@ const setupBase = function (setupIn){
             return false;
         return setup[type];
     };
+    /*
+     * set multiple options interface
+     * @param {object} settings
+     * @public
+     * @return {any}
+     */
     this.setup = function(settings){
+        let out = true;
         for(let i in settings)
-            set(i, settings[i]);
-        return true;
+            if(set(i, settings[i]) === false)
+                out = false;
+        return out;
     };
+    /*
+     * set on option interface
+     * @param {string} type 
+     * @param {any} value
+     * @public
+     * @return {any}
+     */
     this.set = function(type, value){
         if(typeof type === 'undefined')
             return false;
@@ -22,6 +43,13 @@ const setupBase = function (setupIn){
             return false;
         return set(type,value);
     };
+    /*
+     * set on option function
+     * @param {string} type 
+     * @param {any} value
+     * @private
+     * @return {any}
+     */
     let set = function(type, value){
         if(typeof setupTypes[type] === 'undefined')
             return false;
@@ -30,8 +58,20 @@ const setupBase = function (setupIn){
         setup[type] = value;
         return true;
     };
+    /*
+     * @private
+     * @var {object}
+     */
     let setup = {};
+    /*
+     * @private
+     * @var {object}
+     */
     let setupTypes = setupIn;
+    /*
+     * @private
+     * @var {object}
+     */
     for (let i in setupTypes)
         if(typeof setupTypes[i]['default'] !== 'undefined')
             setup[i] = setupTypes[i]['default'];
